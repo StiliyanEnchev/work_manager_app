903316f689d3d60afcbf4b96c3aa185810be3c19
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-11-08 11:37:44 +0200

    Email added to the Add User Admin Form

--- work_manager_app/settings.py
+++ work_manager_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'dashboard',
+    'tasks'
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
