fb5254ca7f3e65d0cceb5d6a25718250ca6056e5
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-11-19 19:46:14 +0200

    Logout option added

--- work_manager_app/settings.py
+++ work_manager_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+
+LOGOUT_REDIRECT_URL = reverse_lazy('home')
