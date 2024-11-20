a2467bab757cd2afc923119d6dc75e083415aeb9
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-11-20 17:52:09 +0200

    Task model created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(4, message='Name must be at least 3 characters')
+        ],
+        unique=True
+    )
+
+    description = models.TextField(validators=[
+        MinLengthValidator(25, message='Description must be at least 25 characters')
+    ])
+
+    done = models.BooleanField(default=False)
+
+    reward = models.IntegerField(
+        validators=[MinValueValidator(1, message='Reward must be at least 1$'), ]
+    )
+    account = models.ForeignKey(
+        to='accounts.CustomUser',
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
