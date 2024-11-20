33ac7ad291498d9e0eac6185b58a8b703fd9b5ac
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-11-20 18:16:28 +0200

    Create Task template and view - created

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from tasks.views import CreateTaskView
+
+urlpatterns = [
+    path('create/', CreateTaskView.as_view(), name='create-task'),
+
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from accounts.models import CustomUser
+from tasks.forms import BaseTaskForm
+from tasks.models import Task
+
 
 # Create your views here.
+class CreateTaskView(CreateView):
+    model = Task
+    template_name = 'task/create.html'
+    form_class = BaseTaskForm
+    success_url = 'home'
