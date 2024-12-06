3dc34c5ee29eef0b453f8cab86dc99b10404082b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-12-06 15:51:46 +0200

    Main title of the tab fixed and page main header shows where you are

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,7 +4,7 @@ from jobs.views import CreateJobView, JobListView, EditJobView
 
 urlpatterns = [
     path('create/', CreateJobView.as_view(), name='create-job'),
-    path('dashoard/', JobListView.as_view(), name='dashboard'),
+    path('dashboard/', JobListView.as_view(), name='dashboard'),
     path('edit/<int:pk>/', EditJobView.as_view(), name='edit-job'),
 
 ]
