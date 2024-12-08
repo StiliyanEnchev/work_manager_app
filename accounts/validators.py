9ef5adf54922d9a246fba104b295d1a03e5bea0a
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-12-08 14:38:42 +0200

    Validator fixed

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
 
 def only_numbers(value):
-    if not str(value).isdigit():
-        raise ValidationError('This field can only contain numeric values.')
+    if value[0] == '+':
+        value = value[1:]
+    if not value.isdigit():
+        raise ValidationError('This field can only contain numeric values and "+" at the beginning.')
