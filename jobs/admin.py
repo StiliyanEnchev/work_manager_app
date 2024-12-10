79b48154ac63899c78e6df4120bb129e1e9f2ca5
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-12-10 19:10:41 +0200

    Jobs added to the admin panel

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from jobs.models import Job
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'salary_per_month', 'owner', 'created_on', 'taken')
+    search_fields = ('title', 'description')
+    list_filter = ('owner', 'salary_per_month')
+
