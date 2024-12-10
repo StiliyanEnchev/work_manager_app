d5c20f6bb1cb9cf13da3e9bea82ef2953a13c414
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-12-10 17:19:23 +0200

    Title of the pages updated to be extracted dynamically from the url

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('details/<int:pk>/', ProfileDetailView.as_view(), name='details'),
-    path('edit/<int:pk>/', ProfileUpdateView.as_view(), name='edit-profile'),
+    path('profile-details/<int:pk>/', ProfileDetailView.as_view(), name='details'),
+    path('edit-profile/<int:pk>/', ProfileUpdateView.as_view(), name='edit-profile'),
     path('password-change/<int:pk>/', ProfilePasswordChange.as_view(), name='password-change'),
     path('delete-profile/<int:pk>/', ProfileDeleteView.as_view(), name='delete-profile'),
 
--- jobs/urls.py
+++ jobs/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from jobs.views import CreateJobView, JobListView, EditJobView, DeleteJobView, JobDetailsView, ApplyForJobView
 
 urlpatterns = [
-    path('create/', CreateJobView.as_view(), name='create-job'),
+    path('create-job/', CreateJobView.as_view(), name='create-job'),
     path('dashboard/', JobListView.as_view(), name='dashboard'),
-    path('edit/<int:pk>/', EditJobView.as_view(), name='edit-job'),
-    path('delete/<int:pk>/', DeleteJobView.as_view(), name='delete-job'),
-    path('details/<int:pk>/', JobDetailsView.as_view(), name='job-details'),
-    path('apply/<int:pk>/', ApplyForJobView.as_view(), name='apply-for-job'),
-    path('successfull/', views.successfull, name='successfull'),
+    path('edit-job/<int:pk>/', EditJobView.as_view(), name='edit-job'),
+    path('delete-job/<int:pk>/', DeleteJobView.as_view(), name='delete-job'),
+    path('job-details/<int:pk>/', JobDetailsView.as_view(), name='job-details'),
+    path('application/<int:pk>/', ApplyForJobView.as_view(), name='apply-for-job'),
+    path('sent/', views.successfull, name='successfull'),
     path('unsuccessfull/', views.unsuccessfull, name='unsuccessfull'),
-    path('candidates/', views.candidates_dashboard, name='candidates')
+    path('my-jobs/', views.candidates_dashboard, name='candidates')
 
 ]
