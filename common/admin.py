52e79672ec55b34782a9c9a701e10312cb8d3091
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-12-11 19:41:43 +0200

    Feedback added to be visible only in the admin panel

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from common.models import News
+from common.models import News, Feedback
 
 
 # Register your models here.
@@ -10,3 +10,11 @@ class NewsAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'published_on')
     search_fields = ('title', 'content')
     ordering = ('-published_on',)
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'created_on')
+    list_filter = ('rating', 'created_on')
+    search_fields = ('user__username', 'message')
+    ordering = ('-created_on',)
